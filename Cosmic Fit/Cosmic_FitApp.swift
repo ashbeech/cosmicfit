7d668ee165f066d3354ead1812819069a8df108a
Ash Beech <ash@bullish.design>
2025-05-04 09:27:59 +0100

    Initial Commit

---
+++ Cosmic Fit/Cosmic_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmic_FitApp.swift
+//  Cosmic Fit
+//
+//  Created by Ashley Davison on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cosmic_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cosmic FitTests/Cosmic_FitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmic_FitTests.swift
+//  Cosmic FitTests
+//
+//  Created by Ashley Davison on 04/05/2025.
+//
+
+import Testing
+@testable import Cosmic_Fit
+
+struct Cosmic_FitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
